fdaa56060319aa11a7a1280329816404b1d5055d
Jan16hcm <tranhoangkhai271625@gmail.com>
2026-07-22 20:50:50 +0700

    idk what I am doing here

--- web/src/login.ts
+++ web/src/login.ts
@@ -1,35 +1,26 @@
 interface validationResult {
-  userError: string;
-  passError: string;
-  isSuccess: boolean;
+  errorMessage: string,
+  isSuccess: boolean
 }
 
-export function validAccount(user: string, pass: string): validationResult {
-  let userError = "";
-  let passError = "";
-  let isSuccess = false;
-  const invalidWord = /[!@#$%^&*(),.?';"{}<>|]/;
+export function validAccount(user: string, pass: string):validationResult {
+  const invalidWord = /[!@#$%^&*(),.?';"{}<>|~`]/;
 
-  if (user === "" || user.length < 3 || invalidWord.test(user)) {
-    userError =
-      "Please enter a username with only letters, numbers, or underscores (3-20 characters).";
+  if(user === "" && pass === "") {
+    return {errorMessage: "Username or password is incorrect. Please try again!", isSuccess: false};
   }
 
-  if (pass === "" || pass.length < 6) {
-    passError = "Password must be at least 6 characters.";
+  if(user === "" || user.length < 3 || invalidWord.test(user)) {
+    return {errorMessage: "Please enter a username with only letters, numbers, or underscores (3-20 characters).", isSuccess: false};
   }
 
-  if (userError === "" && passError === "") {
-    if (user === "binsuong" && pass === "123456") {
-      isSuccess = true;
-    } else {
-      passError = "Username or password is incorrect. Please try again";
-    }
+  if(pass === "" || pass.length < 6) {
+    return {errorMessage: "Password must be at least 6 characters.", isSuccess: false};
   }
 
-  return {
-    userError,
-    passError,
-    isSuccess,
-  };
+  if(user === "binsuong" && pass === "123456") {
+    return {errorMessage: "", isSuccess: true};
+  }
+
+  return {errorMessage: "", isSuccess: false};
 }
--- web/src/main.ts
+++ web/src/main.ts
@@ -7,60 +7,27 @@ const username = document.querySelector<HTMLInputElement>("#username");
 const password = document.querySelector<HTMLInputElement>("#password");
 const toggleBtn = document.querySelector<HTMLButtonElement>("#toggleBtn");
 const form = document.querySelector<HTMLFormElement>("#validForm");
+const errorMessage = document.querySelector<HTMLDivElement>("#errorMessage");
 
-if (form && password && toggleBtn) {
-  toggleBtn.addEventListener("click", () => {
-    const isPassword: boolean = password.type === "password";
-
-    password.type = isPassword ? "text" : "password";
-
-    const eye = toggleBtn.querySelector("i");
-    if (eye) {
-      eye.className = isPassword ? "bi bi-eye-slash" : "bi bi-eye-fill";
-    }
-  });
-}
-
-if (form && username && password) {
+if(form && username && password && toggleBtn && errorMessage) {
   form.addEventListener("submit", (event: Event) => {
     event.preventDefault();
+
     const userValue = username.value.trim();
     const passValue = password.value.trim();
 
-    const result = validAccount(userValue, passValue);
+    const result  = validAccount(userValue, passValue);
 
-    const errorMessage = document.querySelector<HTMLDivElement>("#errorMessage");
+    if(result.isSuccess === false) {
 
-    if (result.userError) {
-      username.classList.add("is-invalid");
-      userFeedBack.textContent = result.userError;
     } else {
-      username.classList.remove("is-invalid");
+      
     }
 
-    if (result.passError) {
-      password.classList.add("is-invalid");
-      passFeedBack.textContent = result.passError;
-    } else {
-      password.classList.remove("is-invalid");
-    }
 
-    if (result.userError) {
-      username.focus();
-    } else if (result.passError) {
-      password.focus();
-    } else if (result.isSuccess) {
-      window.location.assign("../Counter.html");
-    }
-  });
-  username.addEventListener("input", () => {
-    username.classList.remove("is-invalid");
-  });
-
-  password.addEventListener("input", () => {
-    password.classList.remove("is-invalid");
-  });
+  })
 }
+
 
 
 //Counter
