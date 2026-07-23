40efff031a0b472c685d8e890094f34dd5b0ef8b
Jan16hcm <tranhoangkhai271625@gmail.com>
2026-07-23 21:51:20 +0700

    rearrange format: index.html for main menu in the future

--- web/src/counter.ts
+++ web/src/counter.ts
@@ -1,10 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 let count = 0;
-export function countCounter(element: HTMLDivElement, input: number) {
+function countCounter(element: HTMLDivElement, input: number) {
   count += input;
   element.innerHTML = count.toString();
 }
 
-export function resetCounter(element: HTMLDivElement) {
+function resetCounter(element: HTMLDivElement) {
   count = 0;
   element.innerHTML = count.toString();
+}
+
+const counter = document.querySelector<HTMLDivElement>("#counter");
+const increaseBtn = document.querySelector<HTMLButtonElement>("#increaseBtn");
+const decreaseBtn = document.querySelector<HTMLButtonElement>("#decreaseBtn");
+const resetBtn = document.querySelector<HTMLButtonElement>("#resetBtn");
+if (counter && increaseBtn && decreaseBtn && resetBtn) {
+  increaseBtn.addEventListener("click", () => countCounter(counter, 1));
+  decreaseBtn.addEventListener("click", () => countCounter(counter, -1));
+  resetBtn.addEventListener("click", () => resetCounter(counter));
 }
--- web/src/login.ts
+++ web/src/login.ts
@@ -1,3 +1,6 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 interface validationResult {
   errorMessage: string,
   isSuccess: boolean,
@@ -26,3 +29,88 @@ export function validAccount(user: string, pass: string):validationResult {
 
   return {errorMessage: "", isSuccess: true, accountNotFound: true};
 }
+
+const username = document.querySelector<HTMLInputElement>("#username");
+const password = document.querySelector<HTMLInputElement>("#password");
+const toggleBtn = document.querySelector<HTMLButtonElement>("#toggleBtn");
+const eye = toggleBtn?.querySelector<HTMLElement>("i");
+const form = document.querySelector<HTMLFormElement>("#validForm");
+const errorMessage = document.querySelector<HTMLDivElement>("#errorMessage");
+
+function showMessage(
+  errorMessage: HTMLDivElement,
+  text: string,
+  userInput?: HTMLInputElement | undefined,
+  passInput?: HTMLInputElement | undefined,
+): void {
+  errorMessage.innerHTML = text;
+  errorMessage.classList.remove("invisible");
+
+  if (userInput) {
+    userInput.classList.add("is-invalid");
+  }
+  if (passInput) {
+    passInput.classList.add("is-invalid");
+  }
+  if (userInput) {
+    userInput.focus();
+  } else if (passInput) {
+    passInput.focus();
+  }
+}
+if (form && username && password && toggleBtn && errorMessage && eye) {
+  username.addEventListener("input", () => {
+    username.classList.remove("is-invalid");
+    errorMessage.classList.add("invisible");
+  });
+
+  password.addEventListener("input", () => {
+    password.classList.remove("is-invalid");
+    errorMessage.classList.add("invisible");
+  });
+
+  toggleBtn.addEventListener("click", () => {
+    const isPassword: boolean = password.type === "password";
+    password.type = isPassword ? "text" : "password";
+    eye.className = isPassword ? "bi bi-eye-slash" : "bi bi-eye-fill";
+  });
+
+  form.addEventListener("submit", (event: Event) => {
+    event.preventDefault();
+    [username, password].forEach((input) => input.classList.remove("is-invalid"));
+
+    const userValue = username.value.trim();
+    const passValue = password.value.trim();
+
+    const result = validAccount(userValue, passValue);
+
+    if (result.isSuccess === false) {
+      if (result.userError && result.passError && errorMessage) {
+        showMessage(errorMessage, result.errorMessage, username, password);
+
+      } else if (
+        result.userError &&
+        errorMessage
+      ) {
+        showMessage(errorMessage, result.errorMessage, username, undefined);
+
+      } else if (
+        result.passError &&
+        errorMessage
+      ) {
+        showMessage(errorMessage, result.errorMessage, undefined ,password);
+
+      }
+    } else {
+      if (result.accountNotFound) {
+        const notFoundMessage =
+          "The login information you entered is incorrect.";
+        showMessage(errorMessage, notFoundMessage);
+
+      } else {
+        window.location.assign("../Counter.html");
+      }
+    }
+  });
+}
+
